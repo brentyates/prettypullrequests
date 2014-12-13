b1da6c800352364f124377f37a8bc073f291de2c
juandebravo <juandebravo@gmail.com>
2014-12-13 23:04:37 +0100

    support github enterprise

--- background.js
+++ background.js
@@ -1,7 +1,16 @@
-function checkForValidUrl(tabId, changeInfo, tab) {
-    if (tab.url.match("^https://github.com")) {
+
+var gh_enterprise;
+
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    var regExp = gh_enterprise ?
+                "^https:\/\/(github.com|" + gh_enterprise + ")"
+               : "^https:\/\/github.com";
+
+    if (tab.url.match(regExp)) {
         chrome.pageAction.show(tabId);
     }
-}
+});
 
-chrome.tabs.onUpdated.addListener(checkForValidUrl);
+chrome.storage.sync.get({url: ''}, function(items) {
+    gh_enterprise = items.url.replace("https://", "");
+});
