0484b65a22a40990a2090b26ba07a008792f9998
juandebravo <juandebravo@gmail.com>
2014-12-14 10:10:35 +0100

    code review

--- background.js
+++ background.js
@@ -1,16 +1,21 @@
 
 var gh_enterprise;
 
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    var regExp = gh_enterprise ?
-                "^https:\/\/(github.com|" + gh_enterprise + ")"
-               : "^https:\/\/github.com";
+function host(url) {
+    return url.trim().replace(/^(?:https?:\/\/)([^\/?#]+).*$/, '$1').toLowerCase();
+}
 
-    if (tab.url.match(regExp)) {
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+    var urls = gh_enterprise ?
+                gh_enterprise.concat(",").concat("github.com")
+               : "github.com";
+
+    urls = urls.split(',').map(host);
+    if (-1 !== urls.indexOf(host(tab.url))) {
         chrome.pageAction.show(tabId);
     }
 });
 
 chrome.storage.sync.get({url: ''}, function(items) {
-    gh_enterprise = items.url.replace("https://", "");
+    gh_enterprise = items.url;
 });
