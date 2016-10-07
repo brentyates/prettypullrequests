5534fe50d3d1b9f420b96b83615fc7872eda92e6
Evan Salter <esalter@vendasta.com>
2016-10-07 15:01:28 -0600

    Save/load new auto collapse regexs

--- options.js
+++ options.js
@@ -11,14 +11,45 @@
     });
   }
 
+  function addNewAutoCollapseField(value) {
+    var controlForm = $('#auto-collapse-form'),
+        currentEntry = $(controlForm).children('.entry:last'),
+        newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+    newEntry.find('input').val(value);
+    controlForm.find('.entry:not(:last) .btn-add')
+        .removeClass('btn-add').addClass('btn-remove')
+        .removeClass('btn-success').addClass('btn-danger')
+        .html('<span class="glyphicon glyphicon-minus"></span>');
+  }
+
+  function resetAutoCollapseFields() {
+    var controlForm = $('#auto-collapse-form'),
+        currentEntry = $(controlForm).children('.entry:last'),
+        newEntry = $(currentEntry.clone());
+    controlForm.find('.entry').each((index, item) => {
+      item.remove();
+    });
+    newEntry.appendTo(controlForm);
+  }
+
   function setFormValues(options) {
+    resetAutoCollapseFields();
     for (var key in options) {
-      var $formInput = $('#' + key);
+      var $formInput = $('input[name="' + key + '"]');
 
       if ($formInput.prop('type') === 'checkbox') {
         $formInput.prop('checked', options[key]);
       } else {
-        $formInput.prop('value', options[key]);
+        if (options[key].constructor === Array) {
+          var values = options[key];
+          $formInput.val(values.pop())
+          values.forEach(value => {
+            addNewAutoCollapseField(value);
+          });
+        } else {
+          $formInput.prop('value', options[key]);
+        }
       }
     }
   }
@@ -37,7 +68,8 @@
     loadItemsFromChromeStorage({
       url: '',
       saveCollapsedDiffs: true,
-      tabSwitchingEnabled: false
+      tabSwitchingEnabled: false,
+      autoCollapseExpressions: []
       // Add new items here to get them loaded and their values put in the form.
     }, setFormValues);
 
@@ -45,10 +77,19 @@
       e.preventDefault();
 
       var $form = $(e.currentTarget);
-      var url = $form.find('#url').val();
+      var url = $form.find('input[name="url"]').val();
       var origin = url.match(/https?:\/\/[^/?#]+/);
-      var saveCollapsedDiffs = $form.find('#saveCollapsedDiffs').prop('checked');
-      var tabSwitchingEnabled = $form.find('#tabSwitchingEnabled').prop('checked');
+      var saveCollapsedDiffs = $form.find('input[name="saveCollapsedDiffs"]').prop('checked');
+      var tabSwitchingEnabled = $form.find('input[name="tabSwitchingEnabled"]').prop('checked');
+      var autoCollapseExpressions = [];
+
+      $('input[name="autoCollapseExpressions"]').each((index, item) => {
+        autoCollapseExpressions.push($(item).val());
+      });
+      autoCollapseExpressions = autoCollapseExpressions.filter(item => {return item !== ''});
+      if (autoCollapseExpressions.length == 0) {
+        autoCollapseExpressions = [''];
+      }
 
       if (origin) {
         origin[0] += '/*';
@@ -60,7 +101,8 @@
       saveOptionsToChromeStorage({
         'url': url,
         'saveCollapsedDiffs': saveCollapsedDiffs,
-        'tabSwitchingEnabled': tabSwitchingEnabled
+        'tabSwitchingEnabled': tabSwitchingEnabled,
+        'autoCollapseExpressions': autoCollapseExpressions
       }, function () {
           var status = $('#status');
           status.text('Options saved.');
@@ -71,16 +113,8 @@
     });
 
     $(document).on('click', '.btn-add', function(e) {
-      e.preventDefault();
-      var controlForm = $('.controls, #auto-collapse-form'),
-          currentEntry = $(this).parents('.entry:first'),
-          newEntry = $(currentEntry.clone()).appendTo(controlForm);
-
-      newEntry.find('input').val('');
-      controlForm.find('.entry:not(:last) .btn-add')
-          .removeClass('btn-add').addClass('btn-remove')
-          .removeClass('btn-success').addClass('btn-danger')
-          .html('<span class="glyphicon glyphicon-minus"></span>');
+        e.preventDefault;
+        addNewAutoCollapseField('');
     }).on('click', '.btn-remove', function(e){
       $(this).parents('.entry:first').remove();
 
